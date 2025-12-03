065ee6985963e95a22068229e4a9db1b14c401fb
bangde <sheldon@sjconsultinggroup.com>
2025-12-03 15:49:16 +0700

    Update asia-post-shipping.php

--- asia-post-shipping.php
+++ asia-post-shipping.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Plugin Name: Asia Postal & Table Rate Shipping (Fixed)
+ * Plugin Name: Asia Postal & Table Rate Shipping 
  * Plugin URI:  https://example.com/asia-post-shipping
  * Description: A specialized shipping method for Asian Postal Carriers (Japan Post, China Post, Pos Indonesia, etc.) featuring a tree-table rate logic engine.
  * Version:     2.9.4
